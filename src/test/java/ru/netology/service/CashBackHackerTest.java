7fd99d62737a4a0547ec32df8a86c6640cc61582
Kolbin Georgy <magog99@yandex.ru>
2023-02-02 18:08:23 +0400

    add testng

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -2,44 +2,36 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 
 public class CashBackHackerTest {
 
-    CashBackHacker service = new CashBackHacker();
+    @Test
+    public void sholdTestRemainAdd100() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
 
     @Test
-    public void shouldIfAmountUnderLimitBoundary() {
+    public void sholdTestRemainAddZero() {
         CashBackHacker service = new CashBackHacker();
-
-        int amount = 1200;
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
-        int expected = 800;
-
         assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldIfAmountLessLimitBoundary() {
+    public void sholdTestRemainAdd1000() {
         CashBackHacker service = new CashBackHacker();
-
-        int amount = 500;
-        int actual = service.remain(amount);
-        int expected = 500;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldIfAmountEqualsBoundary() {
-        CashBackHacker service = new CashBackHacker();
-
-        int amount = 1000;
-        int actual = service.remain(amount);
+        int amount = 0;
         int expected = 1000;
-
-        assertEquals(expected, actual);
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
     }
-
 }
